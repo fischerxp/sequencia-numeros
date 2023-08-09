344b3121d5359f03c2d71345c17bdd02f2924cee
fischer <108022572+fischerxp@users.noreply.github.com>
2023-08-08 23:55:33 -0300

    c

--- main.c
+++ main.c
@@ -1,35 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Faça um programa onde o usuário irá digitar um número referente aos
-dias da semana. O programa deve identificar qual o dia da semana
-referente ao número digitado. Caso o número digitado não corresponda
-a nenhum dia da semana deve ser exibida uma mensagem indicando
-que o número digitado é inválido.
-1 - Domingo
-2 - Segunda-feira
-3 - Terça-feira
-4 - Quarta-feira
-5 - Quinta-feira
-6 - Sexta-feira
-7 – Sábado*/
+/*Utilizando for escreva a sequência dos números de 100 a 300.*/
 /*desenvolvedor: fischerxp*/
 
 int main()
 {
-    char Dia_Semana;
-    printf ("insira um numero:");
-    scanf ("%c", &Dia_Semana);
-    switch (Dia_Semana)
-    {
-        case '1': printf ( "domingo" ); break;
-        case '2': printf ( "segunda-feira" ); break;
-        case '3': printf ( "terca-feira" ); break;
-        case '4': printf ( "quarta-feira" ); break;
-        case '5': printf ( "quinta-feira" ); break;
-        case '6': printf ( "sexta-feira" ); break;
-        case '7': printf ( "sabado" ); break;
-        default: printf ( "dia invalido" );
-    }
+    int num;
+    for(num = 100; num <= 300; num++)
+    printf("%d\n", num);
+
     return 0;
 }
